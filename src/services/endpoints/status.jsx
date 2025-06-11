950d0bd941b583797c4ecf9cf8258be9759072f7
Alberto <alberto.zimba2013@gmail.com>
2025-06-11 17:10:38 -0300

    feat: status api crud created

--- src/services/endpoints/status.jsx
+++ src/services/endpoints/status.jsx
@@ -2,6 +2,9 @@ import api from "../api";
 
 const statusAPI = {
   getAllStatus: () => api.get("/status/all"),
+  postStatus: (data) => api.post("/status", data),
+  deleteStatus: (data) => api.delete("/status", { data }),
+  patchStatus: (data) => api.patch("/status", data),
 };
 
 export default statusAPI;
