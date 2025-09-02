db4610151e7ab651e13a74c03e4a771afc8c0815
Alberto <alberto.zimba2013@gmail.com>
2025-09-02 13:14:51 -0300

    added prefix in api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 // Configurações da API
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
   TIMEOUT: 1000000,
 };
 
